cf2fe77c3d024a94c3d542a65bfaa39cbad833a2
BaptisteOrieux <baptiste@orieux.net>
2015-11-20 16:22:25 +0100

    Gros trizo d'heras

--- src/plugin/PluginFilter.java
+++ src/plugin/PluginFilter.java
@@ -3,10 +3,18 @@
  */
 package plugin;
 
+import java.io.File;
+import java.io.FilenameFilter;
+
 /**
  * @author orieux
- *
+ * 
  */
-public class PluginFilter {
+public class PluginFilter implements FilenameFilter {
+
+	@Override
+	public boolean accept(File dir, String name) {
+		return name.endsWith(".class");
+	}
 
 }
