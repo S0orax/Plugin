4c9ca2d400b82400a784f7a29398191665b345f8
Heras Guillaume <heras@a12p6.fil.univ-lille1.fr>
2015-11-20 16:06:38 +0100

    Salut yolorieux

---
+++ src/plugin/PluginFinder.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package plugin;
+
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Timer;
+
+/**
+ * @author heras
+ *
+ */
+public class PluginFinder extends PluginFilter {
+	private PluginFilter filter;
+	private Timer timer;
+	private File dir;
+	private ArrayList<String> files;
+	
+	public PluginFinder(String dirPath) {
+		this.filter = new PluginFilter();
+		this.dir = new File(dirPath);
+		this.timer = new Timer(100, new MyActionListener());
+		this.files=new ArrayList<String>();
+		this.timer.start();
+	}
+	
+}
