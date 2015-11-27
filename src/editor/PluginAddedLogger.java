358f645610afed2461a76d3db27037048916f629
S0orax <ttreplay@gmail.com>
2015-11-27 19:40:53 +0100

    Systeme d'évènement terminé

---
+++ src/editor/PluginAddedLogger.java
@@ -0,0 +1,18 @@
+package editor;
+
+import plugins.PluginListener;
+import event.PluginEvent;
+
+public class PluginAddedLogger implements PluginListener {
+
+	@Override
+	public void addingFile(PluginEvent event) {
+		System.out.println("The file  " + event.getFileName() + " was added to the directory " + event.getSource().getDir());
+	}
+
+	@Override
+	public void removingFile(PluginEvent event) {
+		
+	}
+	
+}
--- src/event/PluginEvent.java
+++ src/event/PluginEvent.java
@@ -7,9 +7,20 @@ import plugins.PluginFinder;
 public class PluginEvent extends EventObject {
 
 	private static final long serialVersionUID = 1L;
+	
+	private String fileName;
 
-	public PluginEvent(PluginFinder source) {
+	public PluginEvent(PluginFinder source, String fileName) {
 		super(source);
+		this.fileName = fileName;
+	}
+	
+	public String getFileName() {
+		return this.fileName;
+	}
+	
+	public PluginFinder getSource() {
+		return (PluginFinder) this.source;
 	}
 
 }
