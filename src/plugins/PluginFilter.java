11082016d09e73140eab1550dfb2dbd7896c3cf4
BaptisteOrieux <baptiste@orieux.net>
2015-12-01 10:25:04 +0100

    test

--- src/plugins/PluginFilter.java
+++ src/plugins/PluginFilter.java
@@ -14,14 +14,14 @@ public class PluginFilter implements FilenameFilter {
 
 	@Override
 	public boolean accept(File dir, String name) {
-		if(name.endsWith(".class")) {
+		if (name.endsWith(".class")) {
 			try {
 				Class plugin = Class.forName(name);
 			} catch (ClassNotFoundException e) {
 				System.out.println("Un fichier class non plugin a  été ajouté");
 			}
 		}
-		
+
 		return name.endsWith(".class");
 	}
 
