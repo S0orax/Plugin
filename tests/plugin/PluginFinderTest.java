61eb2c9e965c40024a0e64056ad853f63e3948fa
Heras Guillaume <heras@a16p6.fil.univ-lille1.fr>
2015-12-02 11:48:06 +0100

    PluginFinder Test

--- tests/plugin/PluginFinderTest.java
+++ tests/plugin/PluginFinderTest.java
@@ -3,10 +3,53 @@
  */
 package plugin;
 
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import plugins.PluginFilter;
+import plugins.PluginFinder;
+
 /**
  * @author heras
  *
  */
 public class PluginFinderTest {
+	PluginFinder finder;
+	@Before
+	public void testPluginFinder() {
+		this.finder = new PluginFinder("dropins/plugins");
+	}
+	
+	public void testStartTimer() {
+		this.finder.start();
+		// test remaining time		
+	}
+	
+	public void checkDir() {
+		
+	}
+	public void setandgetFilestest() {
+		ArrayList<String> files = new ArrayList();
+		assertNotNull(this.finder.getFiles());
+	}
+
+	public void getDirtest() {
+		assertNotNull(this.finder.getDir());
+	}
+
+	/**
+	 * Get the filter
+	 * 
+	 * @return the filter
+	 */
+	public void getFiltertest() {
+		assertNotNull(this.finder.getFilter());
+	}
 
 }
--- tests/plugin/TimerActionListenerTest.java
+++ tests/plugin/TimerActionListenerTest.java
@@ -1,5 +1,12 @@
 package plugin;
 
-public class TimerActionListenerTest {
+import plugins.PluginFinder;
+import plugins.TimerActionListener;
+import static org.junit.Assert.*;
 
+public class TimerActionListenerTest {
+	public void TimerActionListenerTest() {
+		PluginFinder finder = new PluginFinder("dropins/plugins");
+		assertNotNull(new TimerActionListener(finder));
+	}
 }
